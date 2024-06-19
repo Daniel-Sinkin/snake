2edc9c8e355f2c4711e5e464a6957d85f9a45383
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-06-19 14:33:21 +0200

    Added the player shader, moved it to top left, started implementing the movement logic

--- src/graphics_engine.py
+++ src/graphics_engine.py
@@ -2,6 +2,7 @@ import os
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 from abc import ABC, abstractmethod
+from enum import Enum, auto
 
 import glm
 import moderngl as mgl
@@ -12,11 +13,21 @@ from moderngl import Context
 from .math import ndc_to_screenspace, screenspace_to_ndc
 
 
+class MoveDirection(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+    NONE = auto()
+
+
 class GraphicsEngine:
     def __init__(self):
         self.window_size: tuple[int, int] = (800, 600)
         self.aspect_ratio: float = self.window_size[0] / self.window_size[1]
         pg.init()
+
+        self.grid_size = 8
 
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
@@ -55,7 +66,7 @@ class GraphicsEngine:
         buffer_format = "2f"
         attributes = ["in_position"]
 
-        vertex_shader = """
+        board_vertex_shader = """
         # version 330
 
         layout(location = 0) in vec2 in_position;
@@ -70,7 +81,7 @@ class GraphicsEngine:
         }
         """
 
-        fragment_shader = """
+        board_fragment_shader = """
         #version 330
 
         const float PI = 3.1415926;
@@ -94,33 +105,137 @@ class GraphicsEngine:
             }
         }
         """
-        self.shader_program = self.ctx.program(
-            vertex_shader=vertex_shader, fragment_shader=fragment_shader
+        self.board_shader_program = self.ctx.program(
+            vertex_shader=board_vertex_shader, fragment_shader=board_fragment_shader
         )
 
-        m_model = glm.mat4()
-        m_model = glm.translate(m_model, glm.vec3(-0.25, 0.0, 0.0))
-        m_model = glm.scale(m_model, glm.vec3(1 / self.aspect_ratio, 1.0, 1.0))
-
-        self.shader_program["m_model"].write(m_model)
-        self.shader_program["i_gridsize"] = 8
-
-        self.quad_vao = self.ctx.vertex_array(
-            self.shader_program, [(quad_vbo, "2f", "in_position")]
+        board_m_model = glm.translate(glm.vec3(-0.25, 0.0, 0.0))
+        board_m_model = glm.scale(
+            board_m_model, glm.vec3(1 / self.aspect_ratio, 1.0, 1.0)
         )
+
+        self.board_shader_program["m_model"].write(board_m_model)
+        self.board_shader_program["i_gridsize"] = self.grid_size
+
+        self.board_vao = self.ctx.vertex_array(
+            self.board_shader_program, [(quad_vbo, "2f", "in_position")]
+        )
+
+        player_vertex_shader = """
+        # version 330
+
+        layout(location = 0) in vec2 in_position;
+
+        uniform mat4 m_model_scale;
+        uniform mat4 m_model_translate;
+
+        out vec2 v_position;
+
+        void main() {
+            gl_Position = m_model_scale * m_model_translate * vec4(in_position, 0.0, 1.0);
+            v_position = in_position;
+        }
+        """
+
+        player_fragment_shader = """
+        #version 330
+
+        const float PI = 3.1415926;
+
+        in vec2 v_position;
+        out vec4 out_color;
+
+        uniform float f_time;
+
+        void main() {
+            out_color = vec4(0.5 - 0.3 * abs(sin(f_time * PI * 3)), 0.7, 0.3, 1.0);
+        }
+        """
+        self.player_shader_program = self.ctx.program(
+            vertex_shader=player_vertex_shader, fragment_shader=player_fragment_shader
+        )
+
+        self.player_m_model_translate = glm.translate(glm.vec3(-9.675, 7.0, 0.0))
+        self.player_m_model_scale = glm.scale(
+            glm.mat4(), glm.vec3(1 / self.aspect_ratio, 1.0, 1.0)
+        )
+        self.player_m_model_scale = glm.scale(
+            self.player_m_model_scale,
+            glm.vec3(1 / self.grid_size, 1 / self.grid_size, 1.0),
+        )
+        self.player_shader_program["m_model_translate"].write(
+            self.player_m_model_translate
+        )
+        self.player_shader_program["m_model_scale"].write(self.player_m_model_scale)
+        self.player_shader_program["f_time"] = 0.0
+
+        self.player_vao = self.ctx.vertex_array(
+            self.player_shader_program, [(quad_vbo, "2f", "in_position")]
+        )
+
+        self.player_grid_position = (0, 0)
+        self.move_direction = MoveDirection.NONE
+        self.previous_move = MoveDirection.NONE
 
     def check_event(self) -> None:
         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.is_running = False
+            match event.type:
+                case pg.QUIT:
+                    self.is_running = False
+                case pg.KEYDOWN:
+                    if event.key in [pg.K_w, pg.K_s, pg.K_a, pg.K_d]:
+                        match event.key:
+                            case pg.K_w:
+                                if self.previous_move != MoveDirection.DOWN:
+                                    self.move_direction = MoveDirection.UP
+                            case pg.K_s:
+                                if self.previous_move != MoveDirection.UP:
+                                    self.move_direction = MoveDirection.DOWN
+                            case pg.K_a:
+                                if self.previous_move != MoveDirection.RIGHT:
+                                    self.move_direction = MoveDirection.LEFT
+                            case pg.K_d:
+                                if self.previous_move != MoveDirection.LEFT:
+                                    self.move_direction = MoveDirection.RIGHT
+
+    def update_gamestate(self):
+        if self.move_direction == MoveDirection.UP:
+            self.player_grid_position = (
+                self.player_grid_position[0],
+                self.player_grid_position[1] + 1,
+            )
+        elif self.move_direction == MoveDirection.DOWN:
+            self.player_grid_position = (
+                self.player_grid_position[0],
+                self.player_grid_position[1] - 1,
+            )
+        elif self.move_direction == MoveDirection.LEFT:
+            self.player_grid_position = (
+                self.player_grid_position[0] - 1,
+                self.player_grid_position[1],
+            )
+        elif self.move_direction == MoveDirection.RIGHT:
+            self.player_grid_position = (
+                self.player_grid_position[0] + 1,
+                self.player_grid_position[1],
+            )
+
+        self.previous_move: MoveDirection = self.move_direction
 
     def update(self) -> None:
-        self.shader_program["f_time"] = self.time
+        print(self.move_direction)
+        self.player_shader_program["m_model_translate"].write(
+            self.player_m_model_translate
+        )
+
+        self.board_shader_program["f_time"] = self.time
+        self.player_shader_program["f_time"] = self.time
 
     def render(self) -> None:
         self.ctx.clear(color=(0.08, 0.16, 0.18))
 
-        self.quad_vao.render(mgl.TRIANGLE_STRIP)
+        self.player_vao.render(mgl.TRIANGLE_STRIP)
+        self.board_vao.render(mgl.TRIANGLE_STRIP)
 
         pg.display.flip()
 
