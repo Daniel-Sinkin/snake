fa5636f0ffa352b536f3322a60cab607e32401af
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-06-20 22:18:22 +0200

    Removed unneeded math.py file

--- src/math.py
+++
@@ -1,7 +0,0 @@
-# type: ignore
-def ndc_to_screenspace(x, y, width, height) -> tuple[int, int]:
-    return int((x + 1.0) / 2.0 * width), int((-y + 1.0) / 2.0 * height)
-
-
-def screenspace_to_ndc(x, y, width, height) -> tuple[float, float]:
-    return 2.0 * x / width - 1.0, 1.0 - 2.0 * y / height
